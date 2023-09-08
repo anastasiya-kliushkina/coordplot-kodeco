a887e4de1e7e7f172c170c12ad3ec6743e7bf6d6
Anastasiya Kliushkina <35305665+anastasiya-kliushkina@users.noreply.github.com>
2023-09-08 16:43:08 -0700

    Added star image for the plot point

--- app/src/main/java/com/kodeco/android/coordplot/Map.kt
+++ app/src/main/java/com/kodeco/android/coordplot/Map.kt
@@ -1,5 +1,6 @@
 package com.kodeco.android.coordplot
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
@@ -9,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
@@ -18,17 +20,15 @@ fun Map(xPercent: Float, yPercent: Float, modifier: Modifier = Modifier) {
     Box(
         modifier
             .size(300.dp)
-            .background(Color.Cyan)
+            .background(Color.Blue)
     ) {
-        Box(
+        Image(
+            painter = painterResource(id = R.drawable.star_icon),
+            contentDescription = "Star point",
             modifier
                 .size(36.dp)
                 .offset((xPercent * 300 - 18).dp, (yPercent * 300 - 18).dp)
-                .clip(shape = CircleShape)
-                .background(Color.Magenta)
-        ) {
-
-        }
+        )
     }
 }
 
